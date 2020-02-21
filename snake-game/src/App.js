558fa777315e08a75105ec4ac90d94c827ad2bb0
Madusha <madusha.pallawela@bbc.co.uk>
2020-02-21 10:16:35 +0000

    Clean up redudant files

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Snake from './Snake';
 
 function App() {
 
+  function keypress({ key }) {
+   console.log(key);
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keypress);
+    return () => {
+      window.addEventListener('keydown', keypress);
+    };
+  }, []);
+
+
   const [snakeDots, setSnake] = useState([
     [0, 0],
-    [2, 0]
+    [2, 0],
+    [4, 0],
+    [6, 0]
   ]);
 
 
+
   return (
-    <div className="game-area">
+    <div className="game-area" > >
       <Snake snake={snakeDots} />
     </div>
   );
