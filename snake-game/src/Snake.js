8bd34d8ccc9a361e181a17dbb5ff3f94fe5d3020
Madusha <madusha.pallawela@bbc.co.uk>
2020-02-23 01:13:01 +0000

    Styled snake head and body

--- snake-game/src/Snake.js
+++ snake-game/src/Snake.js
@@ -1,21 +1,26 @@
-import React, {Component} from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; 
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 
 export default (props) => {
     return (
-    
-          <ReactCSSTransitionGroup
-          transitionName="animation"
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={300}> 
+
+        <ReactCSSTransitionGroup
+            transitionName="animation"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}>
             {props.snake.map((dot, i) => {
                 const style = {
-                    left:`${dot[0]}%`,
-                    top:`${dot[1]}%`
+                    left: `${dot[0]}%`,
+                    top: `${dot[1]}%`
                 }
-                return <div key={[dot[0], dot[1]]} className="snake-dot" style={style}></div>
+                if (props.snake.length - 1 == i) {
+                    return <div key={[dot[0], dot[1]]} className="snake-head" style={style}></div>
+                }
+                else {
+                    return <div key={[dot[0], dot[1]]} className="snake-dot" style={style}></div>
+                }
             })}
-            </ReactCSSTransitionGroup>);
+        </ReactCSSTransitionGroup>);
 }
 
