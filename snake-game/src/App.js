0db127e51c0033439700b177a19180550a454ccb
Madusha <madusha.pallawela@bbc.co.uk>
2020-02-24 16:16:32 +0000

    AI 2.0  - Doesn't move diagonal

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -103,16 +103,19 @@ function App() {
 
   function aiGridAligmnet() {
     let snakeHead = snakeCells.slice(-1)[0]
-
     if (snakeHead[0] > 99) {
       snakeHead[0] -= 2
+      snakeHead[1] += 2
     } else if (snakeHead[0] < 0) {
       snakeHead[0] += 2
+      snakeHead[1] += 2
     }
     else if (snakeHead[1] > 99) {
+      snakeHead[0] -= 2
       snakeHead[1] -= 2
     }
     else if (snakeHead[1] < 0) {
+      snakeHead[0] += 2
       snakeHead[1] += 2
     }
   }
@@ -123,12 +126,11 @@ function App() {
     let distanceX = food[0] - snakeHead[0]
     let distanceY = food[1] - snakeHead[1]
     let snakeTail = updatedCells[0]
-    console.log(distanceX, distanceY)
 
     if (distanceX > 0 || distanceY > 0) {
       if (distanceX != 0) {
         snakeHead[0] += 2
-      }
+      } else
       if (distanceY != 0) {
         snakeHead[1] += 2
       }
@@ -136,7 +138,7 @@ function App() {
     else if (distanceX < 0 || distanceY < 0) {
       if (distanceX != 0) {
         snakeHead[0] -= 2
-      }
+      } else
       if (distanceY != 0) {
         snakeHead[1] -= 2
       }
@@ -144,6 +146,8 @@ function App() {
 
     aiGridAligmnet(snakeHead)
     outOfBoundsCheck(snakeHead)
+
+    //Turn this on when AI doesn't go through it self
     //headBodyCollisionCheck(snakeHead)
 
     if (hasEatenFood(snakeHead)) {
