1cf3fe28d555fa02d54f906146a23488ac8ea857
Madusha <madusha.pallawela@bbc.co.uk>
2020-05-21 14:01:47 +0100

    Updates

---
+++ precache-manifest.2cc77703d4878fbe3706b7d96b39c838.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67afc3cec51a300b5a3446ae17a85dda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a8d7a00e091884ce84",
+    "url": "/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "1c789b46e22c6239954b",
+    "url": "/static/css/main.659a3762.chunk.css"
+  },
+  {
+    "revision": "d4a8d7a00e091884ce84",
+    "url": "/static/js/2.c84db3ef.chunk.js"
+  },
+  {
+    "revision": "c9b1252ec011a7f5998cdd793d0a9627",
+    "url": "/static/js/2.c84db3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c789b46e22c6239954b",
+    "url": "/static/js/main.a5baa0c3.chunk.js"
+  },
+  {
+    "revision": "77a1902f5d4ad5ae20f3",
+    "url": "/static/js/runtime-main.12fecb86.js"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b46d0b9afdee004241226fa9f71581d6.js"
+  "/precache-manifest.2cc77703d4878fbe3706b7d96b39c838.js"
 );
 
 self.addEventListener('message', (event) => {
