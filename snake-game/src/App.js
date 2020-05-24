21885a930f1fab4f1d459f1b042fef5372814765
Madusha <madusha.pallawela@bbc.co.uk>
2020-05-24 16:47:56 +0100

    Use global object to control state for multiple snakes

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -20,84 +20,88 @@ function randomLocation() {
 function App() {
   const [score, setScore] = useState(0)
   const [food, setFood] = useState(randomLocation())
-  const [speed, setSpeed] = useState(100)
+  const [speed, setSpeed] = useState(50)
   const [direction, setDirection] = useState("right")
-  const prevDirection = usePrevious(direction)
   const [aiStatus, setAi] = useState(false)
   const [volume, setVolume] = useState(1)
   const [isGameOver, setGameOver] = useState(false);
   const [showConfetti, setConfetti] = useState(false);
   const [confettiLocation, setConfettiLocation] = useState(food);
 
-  const [snakeCells, setSnake] = useState([
-    { 'x': 0, 'y': 0 },
-    { 'x': 2, 'y': 0 },
-    { 'x': 4, 'y': 0 },
-    { 'x': 6, 'y': 0 },
-  ]);
+  // const [snakeCells, setSnake] = useState([
+  //   { 'x': 0, 'y': 0 },
+  //   { 'x': 2, 'y': 0 },
+  //   { 'x': 4, 'y': 0 },
+  //   { 'x': 6, 'y': 0 },
+  // ]);
 
   const [acronymMap, setAcronymsMap] = useState(acronyms);
   const [currentAcronym, setAcronym] = useState(helper.randomItem(acronymMap))
   const [closeToFood, setCloseToFood] = useState(false);
   const [acronymStatus, setAcronymStatus] = useState(false);
 
-  gamepad.load(setDirection, prevDirection.current)
-  
-  const [snakeCells2, setSnake2] = useState([
-    { 'x': 10, 'y': 10 },
-    { 'x': 12, 'y': 10 },
-    { 'x': 14, 'y': 10 },
-    { 'x': 16, 'y': 10 },
-  ]);
+
   const [direction2, setDirection2] = useState("right")
   const [closeToFood2, setCloseToFood2] = useState(false);
-  const prevDirection2 = usePrevious(direction2)
 
   const [playerCount, setPlayerCount] = useState(0);
 
   const [playerSnakeArray, setPlayerSnakeArray] = useState([
-    {'playerId': '1',
-     'snakeCells': snakeCells,
-    }
+    {
+      playerId: 1,
+      snakeCells: [
+        { 'x': 0, 'y': 0 },
+        { 'x': 2, 'y': 0 },
+        { 'x': 4, 'y': 0 },
+        { 'x': 6, 'y': 0 },
+      ],
+      direction: "right",
+      closeToFood: false
+    },
+    {
+      playerId: 2,
+      snakeCells: [
+        { 'x': 10, 'y': 10 },
+        { 'x': 12, 'y': 10 },
+        { 'x': 14, 'y': 10 },
+        { 'x': 16, 'y': 10 },
+      ],
+      direction: "right",
+      closeToFood: false
+    },
   ]);
+
+  const prevDirection = usePrevious(playerSnakeArray[0].direction)
+  const prevDirection2 = usePrevious(playerSnakeArray[1].direction)
+
+
+  const updateFieldChanged = (index, prop, value) => {
+    let newArr = [...playerSnakeArray];
+    newArr[index][prop] = value;
+
+    setPlayerSnakeArray(newArr);
+  }
 
   useEffect(() => {
     window.addEventListener('keydown', keypress)
     window.addEventListener('keydown', keypress2)
 
-
-    socket.emit('getFood', 'getFood');
-
     socket.on('playerCount', (data) => {
       setPlayerCount(data)
     })
 
-    socket.on('sendFood', (data) => {
-      setFood(data)
-    } )
+    socket.emit('getFood', 'getFood');
+    socket.on('sendFood', (data) => { setFood(data) })
 
-    socket.on('updateDirectionBroadcast', (data) => {
-      setDirection(data)
-    } )
- 
 
-    socket.on('updateBodyBroadcast', (data) => {
-      setSnake(data)
-    } )
-
-    socket.on('updateFoodBroadcast', (data) => {
-      setFood(data)
-    } )
+    socket.on('updateDirectionBroadcast', (data) => { updateFieldChanged(0, 'direction', data) })
+    socket.on('updateBodyBroadcast', (data) => { updateFieldChanged(0, 'snakeCells', data) })
+    socket.on('updateFoodBroadcast', (data) => { setFood(data) })
 
     //Player 2
-    socket.on('updateBodyBroadcast2', (data) => {
-      setSnake2(data)
-    } )
+    socket.on('updateBodyBroadcast2', (data) => { updateFieldChanged(1, 'snakeCells', data) })
+    socket.on('updateDirectionBroadcast2', (data) => { updateFieldChanged(1, 'direction', data) })
 
-    socket.on('updateDirectionBroadcast2', (data) => {
-      setDirection2(data)
-    } )
- 
   }, []);
 
   useEffect(() => {
@@ -105,20 +109,20 @@ function App() {
       if (isGameOver) return;
 
       if (!aiStatus) {
-       tick(snakeCells ,setSnake, direction, setCloseToFood, closeToFood, 1)
-       if(playerCount > 1){    
-         tick(snakeCells2, setSnake2, direction2, setCloseToFood2, closeToFood2, 2)
+        tick(playerSnakeArray[0].snakeCells, playerSnakeArray[0].direction, playerSnakeArray[0].closeToFood, playerSnakeArray[0].playerId - 1)
+        if (playerCount > 1) {
+          tick(playerSnakeArray[1].snakeCells, playerSnakeArray[1].direction, playerSnakeArray[1].closeToFood, playerSnakeArray[1].playerId - 1)
         }
-
-      } else {
-        AI.tick(snakeCells, food, updateBody, setSpeed,
-          setSnake, setDirection, direction, foodCheck, setCloseToFood, closeToFood)
-          AI.tick(snakeCells2, food, updateBody, setSpeed,
-            setSnake2, setDirection2, direction2, foodCheck, setCloseToFood2, closeToFood2)
       }
+      // } else {
+      //   AI.tick(snakeCells, food, updateBody, setSpeed,
+      //     setSnake, setDirection, direction, foodCheck, setCloseToFood, closeToFood)
+      //     AI.tick(snakeCells2, food, updateBody, setSpeed,
+      //       setSnake2, setDirection2, direction2, foodCheck, setCloseToFood2, closeToFood2)
+      // }
     }, speed);
     return () => clearInterval(interval);
-  }, [speed, direction, food, direction2, snakeCells, snakeCells2, setCloseToFood2,closeToFood2,playerSnakeArray, setPlayerSnakeArray ]);
+  }, [speed, direction, food, direction2, playerSnakeArray]);
 
   function usePrevious(value) {
     const ref = useRef();
@@ -133,24 +137,28 @@ function App() {
   function keypress2({ key }) {
     switch (key) {
       case "d":
-        if (prevDirection2.current !== "left")
-          setDirection2("right")
+        if (prevDirection2.current !== "left") {
+          updateFieldChanged(1, "direction", "right")
           socket.emit('updateDirection2', "right");
+        }
         break
       case "a":
-        if (prevDirection2.current !== "right")
-          setDirection2("left")
-          socket.emit('updateDirection2', "left");
+        if (prevDirection2.current !== "right") {
+          updateFieldChanged(1, "direction", "left")
+          socket.emit('updateDirection2', "left")
+        }
         break
       case "s":
-        if (prevDirection2.current !== "up")
-          setDirection2("down")
-          socket.emit('updateDirection2', "down");
+        if (prevDirection2.current !== "up") {
+          updateFieldChanged(1, "direction", "down")
+          socket.emit('updateDirection2', "down")
+        }
         break
       case "w":
-        if (prevDirection2.current !== "down")
-          setDirection2("up")
-          socket.emit('updateDirection2', "up");
+        if (prevDirection2.current !== "down") {
+          updateFieldChanged(1, "direction", "up")
+          socket.emit('updateDirection2', "up")
+        }
         break
       default:
         break;
@@ -160,24 +168,28 @@ function App() {
   function keypress({ key }) {
     switch (key) {
       case "ArrowRight":
-        if (prevDirection.current !== "left")
-          setDirection("right")
-          socket.emit('updateDirection', "right");
+        if (prevDirection.current !== "left") {
+          updateFieldChanged(0, "direction", "right")
+          socket.emit('updateDirection', "right")
+        }
         break
       case "ArrowLeft":
-        if (prevDirection.current !== "right")
-          setDirection("left")
-          socket.emit('updateDirection', "left");
+        if (prevDirection.current !== "right") {
+          updateFieldChanged(0, "direction", "left")
+          socket.emit('updateDirection', "left")
+        }
         break
       case "ArrowDown":
-        if (prevDirection.current !== "up")
-          setDirection("down")
-          socket.emit('updateDirection', "down");
+        if (prevDirection.current !== "up") {
+          updateFieldChanged(0, "direction", "down")
+          socket.emit('updateDirection', "down")
+        }
         break
       case "ArrowUp":
-        if (prevDirection.current !== "down")
-          setDirection("up")
-          socket.emit('updateDirection', "up");
+        if (prevDirection.current !== "down") {
+          updateFieldChanged(0, "direction", "up")
+          socket.emit('updateDirection', "up")
+        }
         break
       default:
         break;
@@ -200,13 +212,13 @@ function App() {
     playSound('game-over.mp3')
   }
 
-  function headBodyCollisionCheck(snakeHead) {
-    let snakeBody = snakeCells.slice(0, -1)
+  // function headBodyCollisionCheck(snakeHead) {
+  //   let snakeBody = snakeCells.slice(0, -1)
 
-    if (helper.isArrayInArray(snakeBody, snakeHead)) {
-      gameOver()
-    }
-  }
+  //   if (helper.isArrayInArray(snakeBody, snakeHead)) {
+  //     gameOver()
+  //   }
+  // }
 
   function outOfBoundsCheck(snakeHead) {
     if (snakeHead.x > 99 || snakeHead.x < 0
@@ -231,23 +243,25 @@ function App() {
     updatedCells.unshift({ 'x': snakeTail.x, 'y': snakeTail.y })
   }
 
-  function handleCloseToFood(snakeHead,setCloseToFood, closeToFood) {
+  function handleCloseToFood(snakeHead, closeToFood, player) {
     let distanceX = Math.abs(food.x - snakeHead.x)
     let distanceY = Math.abs(food.y - snakeHead.y)
 
     if (distanceX < 12 && distanceY < 12) {
+      console.log(closeToFood)
       if (!closeToFood) {
         playSound('mouth.mp3')
       }
-      setCloseToFood(true)
+      updateFieldChanged(player, "closeToFood", true)
     }
     else {
-      setCloseToFood(false)
+      updateFieldChanged(player, "closeToFood", false)
+
     }
   }
 
-  function foodCheck(snakeHead, updatedCells, setCloseTofood, closeToFood) {
-    handleCloseToFood(snakeHead, setCloseTofood, closeToFood)
+  function foodCheck(snakeHead, updatedCells, closeToFood, player) {
+    handleCloseToFood(snakeHead, closeToFood, player)
     if (hasEatenFood(snakeHead)) {
       setConfettiLocation({ 'x': snakeHead.x, 'y': snakeHead.y })
       setConfetti(true)
@@ -266,11 +280,11 @@ function App() {
     }
   }
 
-  function tick(sc, setSnake, direction, setCloseToFood, closeToFood, player) {
-    let updatedCells = updateBody(sc)
+  function tick(snakeCells, direction, closeToFood, player) {
+    let updatedCells = updateBody(snakeCells)
     let snakeHead = updatedCells.slice(-1)[0]
 
-   switch (direction) {
+    switch (direction) {
 
       case "right":
         snakeHead.x += 2;
@@ -290,12 +304,16 @@ function App() {
 
     outOfBoundsCheck(snakeHead)
     // headBodyCollisionCheck(snakeHead)
-    foodCheck(snakeHead, updatedCells, setCloseToFood, closeToFood)
-    setSnake(updatedCells)
-    if(player == 1)
-    socket.emit('updateBody', updatedCells);
-    else 
-    socket.emit('updateBody2', updatedCells);
+    foodCheck(snakeHead, updatedCells, closeToFood, player)
+
+    if (player == 0) {
+      updateFieldChanged(0, 'snakeCells', updatedCells)
+      socket.emit('updateBody', updatedCells);
+    }
+    else {
+      updateFieldChanged(1, 'snakeCells', updatedCells)
+      socket.emit('updateBody2', updatedCells);
+    }
 
 
   }
@@ -305,8 +323,11 @@ function App() {
       <GameOverScreen isGameOver={isGameOver} setGameOver={setGameOver} />
       <ScoreBoard score={score} setAi={setAi} setAcronymStatus={setAcronymStatus} acronymStatus={acronymStatus} aiStatus={aiStatus} setVolume={setVolume} volume={volume} fullWord={currentAcronym.fullWord} />
       <div className="game-area">
-        <Snake num={1} snake={snakeCells} speed={speed} direction={direction} closeToFood={closeToFood} isGameOver={isGameOver} />
-       {(playerCount > 1) ? <Snake num={2} snake={snakeCells2} speed={speed} direction={direction2} closeToFood={closeToFood2} isGameOver={isGameOver} /> : null} 
+
+        {playerSnakeArray.map(player => {
+          return <Snake num={player.playerId} snake={player.snakeCells} speed={speed} direction={player.direction} closeToFood={player.closeToFood} isGameOver={isGameOver} />
+        })}
+
         <Food food={food} confettiLocation={confettiLocation} currentAcronym={currentAcronym} showConfetti={showConfetti} acronymStatus={acronymStatus} />
       </div>
     </div>
